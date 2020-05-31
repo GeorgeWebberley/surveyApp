784c96450565c1281d7d7fc9fa29c8d7083d3b51
George Webberley <georgewebberley@googlemail.com>
2020-05-31 09:15:44 +0100

    Created initial documentation file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import Flask, render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title = "about")
