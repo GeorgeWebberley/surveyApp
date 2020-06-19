e005546fc86822677eb75f4e3dea3ad7eaad2cf9
George Webberley <georgewebberley@googlemail.com>
2020-06-19 10:09:41 +0100

    Added feedback form

--- site/surveyapp/users/forms.py
+++ site/surveyapp/users/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from surveyapp import mongo, login_manager
 
@@ -34,3 +34,16 @@ class UpdateAccountForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+
+
+
+
+
+class FeedbackForm(FlaskForm):
+    user_interface = RadioField('How would you rate the user interface? (1 = bad, 10 = good)',
+    validators=[DataRequired()], choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    functionality = RadioField('How well does the app do what you want it to? (1 = not well, 10 = well)',
+    validators=[DataRequired()], choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    comments = TextAreaField('Any additional comments?')
+    submit = SubmitField('Submit')
