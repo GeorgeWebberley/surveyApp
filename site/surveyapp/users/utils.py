048f53885526ce4ae4591f46468e47200a34ef05
George Webberley <georgewebberley@googlemail.com>
2020-07-12 16:37:38 +0100

    fixed email

--- site/surveyapp/users/utils.py
+++ site/surveyapp/users/utils.py
@@ -9,8 +9,9 @@ def send_email(user):
     print(user.email)
     token = user.get_reset_token()
     message = Message("Reset password request", sender="noreply@datasaur.com", recipients=[user.email])
-    message.body = f"""you have requested a password reset for surveysite.com
+    message.body = f"""You have requested a password reset for datasaur.dev\n
+    Please follow the link to reset your password.\n
     {url_for("users.reset_password", token=token, _external=True)}
-    If you did not request this email then please ignore it!
+    \nIf you did not request this email then please ignore it!
     """
     mail.send(message)
